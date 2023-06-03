59464d16f56b8800b6578939f33feec888ab8459
pinomaker-hoo <inhoo987654321@gmail.com>
2023-06-03 23:34:28 +0900

    feat : 20230603-4

--- 20230603/index.js
+++ 20230603/index.js
@@ -28,8 +28,24 @@ const getMinusIndex = (arr) => {
   return minusIndex;
 };
 
+/**
+ * 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+ */
+const toUpperOrLow = (str) => {
+  const arr = str.split("").map((item) => {
+    if (item === item.toUpperCase()) {
+      return item.toLowerCase();
+    }
+
+    return item.toUpperCase();
+  });
+
+  return arr.join("");
+};
+
 module.exports = {
   getSumCount,
   getArraySlice,
   getMinusIndex,
+  toUpperOrLow,
 };
--- 20230603/index.test.js
+++ 20230603/index.test.js
@@ -1,4 +1,9 @@
-const { getSumCount, getArraySlice, getMinusIndex } = require(".");
+const {
+  getSumCount,
+  getArraySlice,
+  getMinusIndex,
+  toUpperOrLow,
+} = require(".");
 
 test("2023.06.03 1번 문제", () => {
   expect(getSumCount("123456")).toBe(21);
@@ -11,4 +16,8 @@ test("2023.06.03 2번 문제", () => {
 test("2023.06.03 3번 문제", () => {
   expect(getMinusIndex([1, 2, 3, 4, -5])).toBe(4);
   expect(getMinusIndex([1, 2, 3, 4])).toBe(-1);
+});
+
+test("2023.06.03 4번 문제", () => {
+  expect(toUpperOrLow("aBcDeFg")).toEqual("AbCdEfG");
 });
