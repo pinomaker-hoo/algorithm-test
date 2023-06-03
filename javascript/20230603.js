69fad538ec084bd6a7cc9b936634977056de1363
pinomaker-hoo <inhoo987654321@gmail.com>
2023-06-03 15:13:47 +0900

    feat : 20230603-2

--- javascript/20230603.js
+++ javascript/20230603.js
@@ -7,6 +7,14 @@ const getSumCount = (string = "123456") => {
   return arr.reduce((cur, ocr) => cur + Number(ocr), 0);
 };
 
+/**
+ * mission : 정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+ */
+const getArraySlice = (numArr, number) => {
+  return numArr.slice(0, number);
+};
+
 module.exports = {
   getSumCount,
+  getArraySlice,
 };
--- javascript/20230603.test.js
+++ javascript/20230603.test.js
@@ -1,5 +1,9 @@
-const { getSumCount } = require("./20230603");
+const { getSumCount, getArraySlice } = require("./20230603");
 
 test("2023.06.03 1번 문제", () => {
   expect(getSumCount("123456")).toBe(21);
+});
+
+test("2023.06.03 2번 문제", () => {
+  expect(getArraySlice([1, 2, 3, 4], 2)).toStrictEqual([1, 2]);
 });
