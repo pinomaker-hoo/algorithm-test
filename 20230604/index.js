9a9cf4faf4b6541d603ba4da1dbc4422cc053dcf
pinomaker-hoo <inhoo23@naver.com>
2023-06-05 08:45:15 +0900

    feat : 20230604 - 2

--- 20230604/index.js
+++ 20230604/index.js
@@ -16,4 +16,40 @@ const getMinNumber = (A, B) => {
   return sortA.reduce((cur, ocr, index) => cur + ocr * sortB[index], 0);
 };
 
-module.exports = { getMinNumber };
+/**
+ * 짝지어 제거하기는, 알파벳 소문자로 이루어진 문자열을 가지고 시작합니다.
+ * 먼저 문자열에서 같은 알파벳이 2개 붙어 있는 짝을 찾습니다.
+ * 그다음, 그 둘을 제거한 뒤, 앞뒤로 문자열을 이어 붙입니다.
+ * 이 과정을 반복해서 문자열을 모두 제거한다면 짝지어 제거하기가 종료됩니다.
+ * 문자열 S가 주어졌을 때, 짝지어 제거하기를 성공적으로 수행할 수 있는지 반환하는 함수를 완성해 주세요.
+ * 성공적으로 수행할 수 있으면 1을, 아닐 경우 0을 리턴해주면 됩니다.
+ */
+const delStr = (arr) => {
+  let resultArr = [];
+  let result = false;
+  for (const [index, item] of arr.entries()) {
+    if (item === arr[index + 1]) {
+      resultArr = arr.filter((item, i) => i !== index && i !== index + 1);
+      result = true;
+      break;
+    }
+  }
+
+  return result ? resultArr : false;
+};
+
+const getDelStr = (s) => {
+  let arr = s.split("");
+
+  while (true) {
+    const del = delStr(arr);
+    if (!del) {
+      break;
+    }
+    arr = del;
+  }
+
+  return arr.length === 0 ? 1 : 0;
+};
+
+module.exports = { getMinNumber, getDelStr };
--- 20230604/index.test.js
+++ 20230604/index.test.js
@@ -1,5 +1,10 @@
-const { getMinNumber } = require(".");
+const { getMinNumber, getDelStr } = require(".");
 
-test("2023.06.03 1번 문제", () => {
+test("2023.06.04 1번 문제", () => {
   expect(getMinNumber([1, 4, 2], [5, 4, 4])).toBe(29);
+});
+
+test("2023.06.04 2번 문제", () => {
+  expect(getDelStr("baabaa")).toBe(1);
+  expect(getDelStr("cdcd")).toBe(0);
 });
