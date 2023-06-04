79ce1ca4bf5eac97d4a1cd3b89e831733819babd
pinomaker-hoo <inhoo23@naver.com>
2023-06-05 08:47:52 +0900

    feat : 20230605 - 2

--- 20230605/index.js
+++ 20230605/index.js
@@ -5,4 +5,15 @@ const reverseArr = (arr) => {
   return Array.from(String(arr), Number).reverse();
 };
 
-module.exports = { reverseArr };
+/**
+ * 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+ */
+const getNumber = (num) => {
+  if (num % 2 === 0) {
+    return "Even";
+  }
+
+  return "Odd";
+};
+
+module.exports = { reverseArr, getNumber };
--- 20230605/index.test.js
+++ 20230605/index.test.js
@@ -1,5 +1,9 @@
-const { reverseArr } = require(".");
+const { reverseArr, getNumber } = require(".");
 
-test("2023.06.04 1번 문제", () => {
+test("2023.06.05 1번 문제", () => {
   expect(reverseArr(12345)).toEqual([5, 4, 3, 2, 1]);
+});
+
+test("2023.06.05 2번 문제", () => {
+  expect(getNumber(100)).toEqual("Even");
 });
