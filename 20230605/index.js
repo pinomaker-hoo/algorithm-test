541ba2033103d57fa742cbe52cc8ba41606001b1
pinomaker-hoo <inhoo23@naver.com>
2023-06-05 22:27:26 +0900

    feat : 20230605 - 4

--- 20230605/index.js
+++ 20230605/index.js
@@ -36,4 +36,13 @@ const getBetweenSum = (a, b) => {
   return sum;
 };
 
-module.exports = { reverseArr, getNumber, getBetweenSum };
+/**
+ * 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+ * 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+ */
+const getArray = (x, n) => {
+  const arr = new Array(n).fill(x);
+  return arr.map((item, index) => item * (index + 1));
+};
+
+module.exports = { reverseArr, getNumber, getBetweenSum, getArray };
--- 20230605/index.test.js
+++ 20230605/index.test.js
@@ -1,4 +1,4 @@
-const { reverseArr, getNumber, getBetweenSum } = require(".");
+const { reverseArr, getNumber, getBetweenSum, getArray } = require(".");
 
 test("2023.06.05 1번 문제", () => {
   expect(reverseArr(12345)).toEqual([5, 4, 3, 2, 1]);
@@ -10,4 +10,8 @@ test("2023.06.05 2번 문제", () => {
 
 test("2023.06.05 3번 문제", () => {
   expect(getBetweenSum(3, 5)).toEqual(12);
+});
+
+test("2023.06.05 4번 문제", () => {
+  expect(getArray(2, 5)).toEqual([2, 4, 6, 8, 10]);
 });
