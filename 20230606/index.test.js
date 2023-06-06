a099ab1a74734b514463fccffe7381e9bd634aa7
pinomaker-hoo <inhoo23@naver.com>
2023-06-06 19:18:05 +0900

    feat : 20230606 - 3

--- 20230606/index.test.js
+++ 20230606/index.test.js
@@ -1,4 +1,4 @@
-const { getDiviorArr, getCount } = require(".");
+const { getDiviorArr, getCount, delMixIndex } = require(".");
 
 test("2023.06.06 1번 문제", () => {
   expect(getDiviorArr([5, 9, 7, 10], 5)).toEqual([5, 10]);
@@ -8,4 +8,9 @@ test("2023.06.06 2번 문제", () => {
   expect(getCount("pPoooyY")).toEqual(true);
   expect(getCount("Pyy")).toEqual(false);
   expect(getCount("b")).toEqual(true);
+});
+
+test("2023.06.06 3번 문제", () => {
+  expect(delMixIndex([4, 3, 2, 1])).toEqual([4, 3, 2]);
+  expect(delMixIndex([10])).toEqual([-1]);
 });
