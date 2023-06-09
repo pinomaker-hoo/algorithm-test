8973a0fcae44e4f9d45242da8f18693d1aa21a7d
pinomaker-hoo <inhoo23@naver.com>
2023-06-09 16:06:29 +0900

    feat : 20230609 - 4

--- 20230609/index.js
+++ 20230609/index.js
@@ -51,8 +51,21 @@ const getPhone = (phone_number) => {
   return first.join("") + arr.slice(-4).join("");
 };
 
+/**
+ * 어떤 정수들이 있습니다.
+ * 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다.
+ * 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+ */
+const getNumber = (absolutes, signs) => {
+  return absolutes.reduce((cur, ocr, index) => {
+    const number = signs[index] ? ocr : ocr * -1;
+    return cur + number;
+  }, 0);
+};
+
 module.exports = {
   getMoeny,
   getCollatz,
   getPhone,
+  getNumber,
 };
--- 20230609/index.test.js
+++ 20230609/index.test.js
@@ -1,4 +1,4 @@
-const { getMoeny, getCollatz, getPhone } = require(".");
+const { getMoeny, getCollatz, getPhone, getNumber } = require(".");
 
 test("2023.06.09 1번 문제", () => {
   expect(getMoeny(3, 20, 4)).toEqual(10);
@@ -10,6 +10,10 @@ test("2023.06.09 2번 문제", () => {
   expect(getCollatz(626331)).toEqual(-1);
 });
 
-test("2023.06.09 2번 문제", () => {
+test("2023.06.09 3번 문제", () => {
   expect(getPhone("01033334444")).toEqual("*******4444");
+});
+
+test("2023.06.09 4번 문제", () => {
+  expect(getNumber([4, 7, 12], [true, false, true])).toEqual(9);
 });
