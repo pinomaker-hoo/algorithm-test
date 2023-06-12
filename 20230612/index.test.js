2c05446fbcf595c81f9851c1f21db07195ad6eae
pinomaker-hoo <inhoo23@naver.com>
2023-06-12 23:45:13 +0900

    feat : 20230612 - 3

--- 20230612/index.test.js
+++ 20230612/index.test.js
@@ -1,4 +1,4 @@
-const { getWaterMelon, getCount } = require(".");
+const { getWaterMelon, getCount, getSum } = require(".");
 
 test("2023.06.12 1번 문제", () => {
   expect(getWaterMelon(3)).toEqual("수박수");
@@ -7,4 +7,22 @@ test("2023.06.12 1번 문제", () => {
 
 test("2023.06.12 2번 문제", () => {
   expect(getCount([1, 2, 3, 4], [-3, -1, 0, 2])).toEqual(3);
+});
+
+test("2023.06.12 3번 문제", () => {
+  expect(
+    getSum(
+      [
+        [1, 2],
+        [2, 3],
+      ],
+      [
+        [3, 4],
+        [5, 6],
+      ]
+    )
+  ).toEqual([
+    [4, 6],
+    [7, 9],
+  ]);
 });
