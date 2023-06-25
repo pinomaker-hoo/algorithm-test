82bd3ab1085e766004939ae9c2c9aa9a51255812
pinomaker-hoo <inhoo23@naver.com>
2023-06-25 13:01:36 +0900

    feat : 20230625 - 2

--- 20230625/index.test.js
+++ 20230625/index.test.js
@@ -1,5 +1,9 @@
-const { sortStr } = require(".");
+const { sortStr, logStar } = require(".");
 
 test("2023.06.25 1번 문제", () => {
   expect(sortStr("Zbcdefg")).toEqual("gfedcbZ");
 });
+
+// test("2023.06.25 2번 문제", () => {
+//   expect(logStar(5, 1)).toEqual("*****");
+// });
